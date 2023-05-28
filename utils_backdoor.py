486701cf7d1eaf61043220b3195169f745b13ad2
Bartlomiej_Marek <bart.marek00@gmail.com>
2023-05-28 23:01:43 +0000

    Update utils_backdoor.py

--- utils_backdoor.py
+++ utils_backdoor.py
@@ -1,4 +1,7 @@
 from copy import deepcopy
+import numpy as np
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.metrics import classification_report
 
 def calculate_attack_success_rate(predicted, with_trigger, target_class):
     with_trigger = predicted[np.where(with_trigger == 1)]
@@ -42,6 +45,40 @@ def run_cv_backdoor(classifier, params, name, with_trigger, trigger_size, trigge
                 'Support': metrics['support'],
                 'ASR': asr,
                 'TAP': 100 * round(trigger_size / number_of_features, 3)
+            }
+            for label, metrics in report.items()
+            if label.isdigit()
+        )
+    return results
+
+
+def run_cv(X, y, classifier, params, name):
+    results = []
+    rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=368)
+
+    for fold_no, (train_idx, test_idx) in enumerate(rskf.split(X, y)):
+        model = classifier(**params)
+
+        if name == 'Neural Network':
+            model.fit(X[train_idx], y[train_idx], epochs=10, batch_size=32, verbose=0)
+
+        else:
+            model.fit(X[train_idx], y[train_idx])
+
+        y_pred = model.predict(X[test_idx])
+
+        # Generate classification report
+        report = classification_report(y[test_idx], y_pred, output_dict=True)
+
+        results.extend(
+            {
+                'Method': name,
+                'Fold': fold_no,
+                'Class': int(label),
+                'Precision': metrics['precision'],
+                'Recall': metrics['recall'],
+                'F1-score': metrics['f1-score'],
+                'Support': metrics['support'],
             }
             for label, metrics in report.items()
             if label.isdigit()
